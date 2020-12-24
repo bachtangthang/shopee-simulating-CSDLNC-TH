bcb8217316761c5462a21fe2dd834b14beaeaa60
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2020-12-24 20:44:07 +0700

    Update shopee_fix.sql

--- shopee_fix.sql
+++ shopee_fix.sql
@@ -805,22 +805,3 @@ ALTER TABLE dbo.SANPHAMCHON
 	ADD CONSTRAINT fk_SANPHAMCHON_CO_VOUCHERSHOP FOREIGN KEY (MAVOUCHER)
 		REFERENCES dbo.VOUCHERSHOP(MAVOUCHER)
 		
---Chọn sản phẩm để cập nhật
-create proc sp_ChonSanPhamCapNhat(@tensp nvarchar(20))
-as
-begin
-	select MAMATHANG from MATHANG where TENMATHANG=@tensp
-end
-
---Cap nhat san pham
---create
-alter proc sp_CapNhatSanPham(@masp nvarchar(20), @mota nvarchar(20), 
-@cd float, @cr float, @cc float, @giavanchuyen float, @dathang bit,
-@tinhtrang nvarchar(50), @ngaychuanbi int, @khoiluong float, @tensp nvarchar(20))
-as
-begin
-	update MATHANG set MOTASANPHAM=@mota, CHIEUCAO=@cc, CHIEUDAI=@cd, CHIEURONG=@cr, 
-	GIAVANCHUYENDUKIEN=@giavanchuyen, DATHANGTRUOC =@dathang, TINHTRANG=@tinhtrang, 
-	SONGAYCANCHUANBI=@ngaychuanbi, KHOILUONGDONGGOI=@khoiluong, TENMATHANG=@tensp
-	where MAMATHANG=@masp
-end
