2111c85079c7ae56d1ef20dc6764109a517ba5ee
NhatHiep-isme <55236520+NhatHiep-isme@users.noreply.github.com>
2020-12-24 09:06:45 +0700

    Update shopee_fix.sql

--- shopee_fix.sql
+++ shopee_fix.sql
@@ -813,12 +813,14 @@ begin
 end
 
 --Cap nhat san pham
-create proc sp_CapNhatSanPham(@masp nvarchar(20), @mota nvarchar(20), 
+--create
+alter proc sp_CapNhatSanPham(@masp nvarchar(20), @mota nvarchar(20), 
 @cd float, @cr float, @cc float, @giavanchuyen float, @dathang bit,
-@tinhtrang nvarchar(50), @ngaychuanbi int)
+@tinhtrang nvarchar(50), @ngaychuanbi int, @khoiluong float, @tensp nvarchar(20))
 as
 begin
 	update MATHANG set MOTASANPHAM=@mota, CHIEUCAO=@cc, CHIEUDAI=@cd, CHIEURONG=@cr, 
-	GIAVANCHUYENDUKIEN=@giavanchuyen, DATHANGTRUOC =@dathang, TINHTRANG=@tinhtrang, SONGAYCANCHUANBI=@ngaychuanbi
+	GIAVANCHUYENDUKIEN=@giavanchuyen, DATHANGTRUOC =@dathang, TINHTRANG=@tinhtrang, 
+	SONGAYCANCHUANBI=@ngaychuanbi, KHOILUONGDONGGOI=@khoiluong, TENMATHANG=@tensp
 	where MAMATHANG=@masp
 end
