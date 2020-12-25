1055337abd5d7830905c2a6c656035464dbc668d
bachtangthang <51031544+bachtangthang@users.noreply.github.com>
2020-12-26 00:22:22 +0700

    mô hình 3 lớp

---
+++ shoppe/shoppe/DAO/MatHangDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppe.DAO
+{
+    public class MatHangDAO
+    {
+        private static MatHangDAO instance;
+
+        public static MatHangDAO Instance
+        {
+            get { if (instance == null) instance = new MatHangDAO(); return MatHangDAO.instance; }
+            private set { MatHangDAO.instance = value; }
+        }
+    }
+}
---
+++ shoppe/shoppe/DAO/SanPhamChonDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoppe.DAO
+{
+    class SanPhamChonDAO
+    {
+        private static SanPhamChonDAO instance;
+
+        public static SanPhamChonDAO Instance
+        {
+            get { if (instance == null) instance = new SanPhamChonDAO(); return SanPhamChonDAO.instance; }
+            private set { SanPhamChonDAO.instance = value; }
+        }
+    }
+}
