80ead320dd325434ab838cd97add65d243d337c7
An Pham <anphamimpala@gmail.com>
2020-12-26 00:34:19 +0700

    up ho ban Hiep <3333

--- shoppe/shoppe/Form1.Designer.cs
+++ shoppe/shoppe/Form1.Designer.cs
@@ -29,13 +29,79 @@ namespace shoppe
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.tbTK = new System.Windows.Forms.TextBox();
+            this.tbMK = new System.Windows.Forms.TextBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = null;
+            this.pictureBox1.InitialImage = null;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(898, 477);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.UseWaitCursor = true;
+            this.pictureBox1.WaitOnLoad = true;
+            // 
+            // tbTK
+            // 
+            this.tbTK.Location = new System.Drawing.Point(504, 117);
+            this.tbTK.Name = "tbTK";
+            this.tbTK.Size = new System.Drawing.Size(259, 22);
+            this.tbTK.TabIndex = 1;
+            // 
+            // tbMK
+            // 
+            this.tbMK.Location = new System.Drawing.Point(504, 174);
+            this.tbMK.Name = "tbMK";
+            this.tbMK.Size = new System.Drawing.Size(259, 22);
+            this.tbMK.TabIndex = 2;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.White;
+            this.btnLogin.Location = new System.Drawing.Point(504, 224);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(259, 38);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "ĐĂNG NHẬP";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(902, 478);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.tbMK);
+            this.Controls.Add(this.tbTK);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.Color.Coral;
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox tbTK;
+        private System.Windows.Forms.TextBox tbMK;
+        private System.Windows.Forms.Button btnLogin;
+
     }
 }
 
--- shoppe/shoppe/Form1.cs
+++ shoppe/shoppe/Form1.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace shoppe
 {
@@ -15,6 +17,37 @@ namespace shoppe
         public Form1()
         {
             InitializeComponent();
+            pictureBox1.Image = new Bitmap(Application.StartupPath + "\\image\\94557138_1204930936504722_3329135653433638912_n.png");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-KMNS09Q\SQLEXPRESS;Initial Catalog=Account;Integrated Security=True");
+            try
+            {
+                conn.Open();
+                string tk=tbTK.Text;
+                string mk=tbMK.Text;
+                string sql="select * from NguoiDung where TaiKhoan= '"+tk+"' and MatKhau='"+mk+"'";
+                SqlCommand cmd=new SqlCommand(sql, conn);
+                SqlDataReader dta=cmd.ExecuteReader();
+                if(dta.Read()==true)
+                {
+                    MessageBox.Show("Đăng nhập thành công!");
+                }
+                else{
+                    MessageBox.Show("Đăng nhập thất bại!");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Lỗi kết nối!");
+            }
         }
     }
 }
