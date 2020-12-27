7804e23b6db7545a4bc11cc0700a79d65bce53fa
TeoLeoGit <59004156+TeoLeoGit@users.noreply.github.com>
2020-12-27 15:02:34 +0700

    Kiểm tra điều kiện trong tigger, thêm file TriggerChoProcedureCuaCuong

--- ProcedureCuaBanCuong.sql
+++ ProcedureCuaBanCuong.sql
@@ -3,6 +3,7 @@ go
 
 --Quy trình thêm hàng: gọi Procedure Them_hang --> Them_thong_tin_van_chuyen --> Them_nhom_nganh_hang --> Them_lua_chon
 --Thêm hàng
+
 CREATE PROCEDURE Them_hang
 	@p_MATAIKHOAN nvarchar(10),
 	@p_MASHOP nvarchar(10),
@@ -18,26 +19,6 @@ CREATE PROCEDURE Them_hang
 	@p_SONGAYCANCHUANBI int
 AS
 BEGIN
-	IF (@p_CHIEUCAO <= 0 OR @p_CHIEUCAO > 1000000)
-		BEGIN
-			RAISERROR ('Chieu cao khong hop le.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
-	IF (@p_CHIEUDAI <= 0 OR @p_CHIEUDAI > 1000000)
-		BEGIN
-			RAISERROR ('Chieu dai khong hop le.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
-	IF (@p_CHIEURONG <= 0 OR @p_CHIEURONG > 1000000)
-		BEGIN
-			RAISERROR ('Chieu rong khong hop le.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
-	IF (@p_KHOILUONGDONGGOI <=0 OR @p_KHOILUONGDONGGOI > 100)
-		BEGIN
-			RAISERROR ('Khoi luong dong goi vuot qua gioi han hoac khong hop le.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
 	DECLARE @v_GenIndentifier NVARCHAR(10)
 	--Identifier: Đổi timestamp(ngày - giờ - phút - giây) ra string và nối vào 5 chữ đầu của mã shop;
 	SET @v_GenIndentifier = CONVERT(NVARCHAR(MAX), CONVERT(BINARY(8), SUBSTRING(CONVERT(timestamp, GETDATE()),3,20)), 2) + SUBSTRING(@p_MASHOP, 1, 5);
@@ -53,16 +34,7 @@ CREATE PROCEDURE Them_thong_tin_nhom_nganh_hang
 	@p_TENNHOMHANG nvarchar(10)
 AS
 BEGIN
-	--kiểm tra đã thêm MATHANG thành công chưa
-	IF (NOT EXISTS(SELECT MAMATHANG
-					FROM MATHANG
-					WHERE MAMATHANG = @p_MAMATHANG))
-		BEGIN
-			RAISERROR ('Khong tim thay mat hang.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
-	ELSE
-		INSERT INTO NHOMNGANHHANG VALUES (@p_MANHOM, @p_MAMATHANG, @p_TENNHOMHANG)
+	INSERT INTO NHOMNGANHHANG VALUES (@p_MANHOM, @p_MAMATHANG, @p_TENNHOMHANG)
 END;
 GO
 
@@ -72,15 +44,7 @@ CREATE PROCEDURE Them_thong_tin_van_chuyen
 	@p_MADONVI nvarchar(10)
 AS
 BEGIN
-	IF (NOT EXISTS(SELECT MAMATHANG
-					FROM MATHANG
-					WHERE MAMATHANG = @p_MAMATHANG))
-		BEGIN
-			RAISERROR ('Khong tim thay mat hang.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
-	ELSE
-		INSERT INTO VAN_CHUYEN_MAT_HANG VALUES (@p_MAMATHANG, @p_MADONVI)
+	INSERT INTO VAN_CHUYEN_MAT_HANG VALUES (@p_MAMATHANG, @p_MADONVI)
 END;
 GO
 
@@ -94,23 +58,6 @@ CREATE PROCEDURE Them_lua_chon
 	@p_SKUPHANLOAI nvarchar(10)
 AS
 BEGIN
-	IF (NOT EXISTS(SELECT MAMATHANG
-					FROM MATHANG
-					WHERE MAMATHANG = @p_MAMATHANG))
-		BEGIN
-			RAISERROR ('Khong tim thay mat hang.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
-	IF (@p_GIACA < 1000 OR @p_GIACA > 120000000)
-		BEGIN
-			RAISERROR ('Gia ca khong hop le.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
-	IF (@p_SOLUONGTONKHO < 0 OR @p_SOLUONGTONKHO > 999999)
-		BEGIN
-			RAISERROR ('Gia ca khong hop le.', 16, 1)
-			ROLLBACK TRANSACTION
-		END
 	DECLARE @v_GenMALUACHON nvarchar(10)
 	SET @v_GenMALUACHON = CAST(NEWID() as nvarchar(40)) + SUBSTRING(@p_MAMATHANG, 1, 5);
 	INSERT INTO LUACHON VALUES (@p_MAMATHANG, @p_TENCUMPHANLOAI, @p_TENLUACHON, @v_GenMALUACHON, @p_SOLUONGTONKHO, @p_GIACA, @p_SKUPHANLOAI)
---
+++ TriggerChoProcedureCuaCuong.sql
@@ -0,0 +1,62 @@
+﻿USE test1
+GO
+
+--Trigger kiểm tra MATHANG khi INSERT
+CREATE TRIGGER Check_Insert_MatHang
+ON MATHANG
+FOR INSERT
+AS
+BEGIN
+	IF (EXISTS(SELECT *
+				FROM inserted
+				WHERE inserted.CHIEUCAO <= 0 OR inserted.CHIEUCAO > 1000000))
+		BEGIN
+			RAISERROR ('Chieu cao khong hop le.', 16, 1)
+			ROLLBACK TRANSACTION
+		END
+	IF (EXISTS(SELECT *
+				FROM inserted
+				WHERE inserted.CHIEUDAI <= 0 OR inserted.CHIEUDAI > 1000000))
+		BEGIN
+			RAISERROR ('Chieu dai khong hop le.', 16, 1)
+			ROLLBACK TRANSACTION
+		END
+	IF (EXISTS(SELECT *
+				FROM inserted
+				WHERE inserted.CHIEURONG <= 0 OR inserted.CHIEURONG > 1000000))
+		BEGIN
+			RAISERROR ('Chieu rong khong hop le.', 16, 1)
+			ROLLBACK TRANSACTION
+		END
+	IF (EXISTS(SELECT *
+				FROM inserted
+				WHERE inserted.KHOILUONGDONGGOI <= 0 OR inserted.KHOILUONGDONGGOI > 1000000))
+		BEGIN
+			RAISERROR ('Khoi luong dong goi vuot qua gioi han hoac khong hop le.', 16, 1)
+			ROLLBACK TRANSACTION
+		END
+END;
+GO
+
+--Trigger kiểm tra thông tin lựa chọn
+CREATE TRIGGER Check_Insert_LuaChon
+ON LUACHON
+FOR INSERT
+AS
+BEGIN
+	IF (EXISTS(SELECT *
+				FROM inserted
+				WHERE inserted.GIACA < 1000 OR inserted.GIACA > 120000000))
+		BEGIN
+			RAISERROR ('Gia ca khong hop le.', 16, 1)
+			ROLLBACK TRANSACTION
+		END
+	IF (EXISTS(SELECT *
+				FROM inserted
+				WHERE inserted.SOLUONGTONKHO < 0 OR inserted.SOLUONGTONKHO > 999999))
+		BEGIN
+			RAISERROR ('So luong ton kho khong hop le.', 16, 1)
+			ROLLBACK TRANSACTION
+		END
+END;
+GO
